87c2ef8fe6873cd19cb9e9d1e2d938d577ccc24c
scott terry <vireodata@outlook.com>
2017-08-02 22:25:00 -0700

    add minification to prod server code

--- Vireo.Ui.Apptivator/server/data/collections/index.js
+++ Vireo.Ui.Apptivator/server/data/collections/index.js
@@ -1,3 +1,3 @@
-﻿export { default as customersCollection } from "customersCollection";
-export { default as chargesCollection } from "chargesCollection";
-export { default as appContentsCollection } from "appContentsCollection";
+﻿export { default as customersCollection } from "./customersCollection";
+export { default as chargesCollection } from "./chargesCollection";
+export { default as appContentsCollection } from "./appContentsCollection";
--- Vireo.Ui.Apptivator/web/webpack.config.prod.js
+++ Vireo.Ui.Apptivator/web/webpack.config.prod.js
@@ -87,7 +87,8 @@ module.exports = [{
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
-        })
+        }), ,
+        new webpack.optimize.UglifyJsPlugin()
     ],
     cache: true
 }]
