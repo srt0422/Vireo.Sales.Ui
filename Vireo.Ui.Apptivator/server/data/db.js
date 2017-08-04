3be7497c52f9f3dca2bd43bb87b12eed900de11f
scott terry <vireodata@outlook.com>
2017-08-04 07:34:33 -0700

    update mongo connection

--- Vireo.Ui.Apptivator/server/data/db.js
+++ Vireo.Ui.Apptivator/server/data/db.js
@@ -1,6 +1,6 @@
 ﻿import { MongoClient } from "mongodb";
 
-const url = process.env.NODE_ENV === "production" ? 'mongodb://40.78.96.177:27017/apptivator' : 'mongodb://localhost:27017/apptivator';
+const url = process.env.NODE_ENV === "production" ? 'mongodb://40.78.96.177:2000/apptivator' : 'mongodb://localhost:27017/apptivator';
 
 let db = null;
 
