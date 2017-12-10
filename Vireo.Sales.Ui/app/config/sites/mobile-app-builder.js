97faa3439ca92b86a3196a64f5f1d89563b31c22
unknown <vireodata@outlook.com>
2017-12-09 21:16:07 -0800

    update content

--- Vireo.Sales.Ui/app/config/sites/mobile-app-builder.js
+++ Vireo.Sales.Ui/app/config/sites/mobile-app-builder.js
@@ -2,7 +2,7 @@ export default {
     // home
     comanyTitle: "Vireo",
     companyLogoUrl: "https://s-media-cache-ak0.pinimg.com/originals/be/cc/9e/becc9e2160b760a4918c321c6aacdc28.png",
-    homeHeading: "Easy and Affordable Mobile App Services for Small Businesses",
+    homeHeading: "Easy and Affordable Mobile App Builder for Small Businesses",
     homeContent: "In a matter of minutes you can have a custom mobile application for your business.  With billions of people searching for apps in the app store, your business is missing out as long as you don't have an app exposing mobile users to your company.  ",
     offers: [{
             title: "Custom Apple Business App",
@@ -19,6 +19,6 @@ export default {
             offerKey: "app_builder"
         }],
     //contact
-    contactHeading: "You're on your way to more exposure and customer engagement!",
+    contactHeading: "You're on your way to the world of mobile!",
     contactContent: "Please enter the details of your request below.  Then, with one easy payment, we'll get you started with your selected package.  You will be notified when your payment has been processed.  We will contact you thereafter with first steps."
 };
